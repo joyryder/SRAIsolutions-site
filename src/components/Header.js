d0c893e0282da7c495f18c532ca61f00b793ad3f
Shrif Rai <shrifrai@Shrifs-MacBook-Pro.local>
2023-01-31 21:30:46 -0500

    fixed mission text grey-ed out bug

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ function Header() {
     <header>
       <video src="./video.mp4" loop autoPlay muted></video>
       <h1>AI Technologies</h1>
-      <p style={{textAlign: "center", color:"white", width:"80%", fontSize:"2em"}}>
+      <p style={{textAlign: "center", color:"white", width:"80%", fontSize:"2em", zIndex:7}}>
         Our goal is to create open-source AI projects through interdisciplinary 
         research collaborations between leaders and subject-matter experts, 
         including researchers, engineers, developers, PhD candidates, and AI artists.
